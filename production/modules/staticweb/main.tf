b56365cdfbd8c53037d1344434f2188c2a618b5b
ArerepadeB <aredelulu@gmail.com>
2022-03-06 15:39:27 -0600

    updating

--- production/modules/staticweb/main.tf
+++ production/modules/staticweb/main.tf
@@ -14,10 +14,24 @@ resource "aws_s3_bucket" "bucket" {
   website {
     index_document = "index.html"
     error_document = "404.html"
+    
   }
 
   tags = merge(var.tags, { Name = "elite-mainbucket" })
 }
+
+resource "aws_s3_bucket" "bucket_redirect" {
+  bucket = "www.${var.bucket_name}-redirect"
+  acl    = "public-read"
+
+  website {
+   redirect_all_requests_to = "https://${var.website-domain}"
+    
+  }
+
+  tags = merge(var.tags, { Name = "elite-mainbucket" })
+}
+
 
 # Creates policy to allow public access to the S3 bucket
 resource "aws_s3_bucket_policy" "bucket_policy" {
@@ -37,6 +51,30 @@ resource "aws_s3_bucket_policy" "bucket_policy" {
       ],
       "Resource": [
         "${aws_s3_bucket.bucket.arn}/*"
+      ]
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_s3_bucket_policy" "bucket_policy_redirect" {
+  bucket = aws_s3_bucket.bucket_redirect.id
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Id": "PolicyForWebsiteEndpointsPublicContent",
+  "Statement": [
+    {
+      "Sid": "PublicRead",
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": [
+        "s3:GetObject"
+      ],
+      "Resource": [
+        "${aws_s3_bucket.bucket_redirect.arn}/*"
       ]
     }
   ]
